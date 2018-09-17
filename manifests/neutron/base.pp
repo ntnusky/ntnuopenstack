0fd7176a9b4b3230ba3fe2596951da368920459a
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-17 13:04:47 +0200

    enable ha queues for neutron

--- manifests/neutron/base.pp
+++ manifests/neutron/base.pp
@@ -2,10 +2,20 @@
 class ntnuopenstack::neutron::base {
   $service_plugins = hiera('ntnuopenstack::neutron::service_plugins')
   $mtu = hiera('ntnuopenstack::neutron::mtu', undef)
+  $rabbitservers = hiera('profile::rabbitmq::servers', false)
   $transport_url = hiera('ntnuopenstack::transport::url')
 
   require ::ntnuopenstack::repo
   include ::ntnuopenstack::neutron::sudo
+
+  if ($rabbitservers) {
+    $ha_transport_conf = {
+      rabbit_ha_queues    => true,
+      amqp_durable_queues => true,
+    }
+  } else {
+    $ha_transport_conf = {}
+  }
 
   class { '::neutron':
     allow_overlapping_ips   => true,
@@ -14,5 +24,6 @@ class ntnuopenstack::neutron::base {
     dhcp_agents_per_network => 2,
     global_physnet_mtu      => $mtu,
     service_plugins         => $service_plugins,
+    *                       => $ha_transport_conf,
   }
 }
