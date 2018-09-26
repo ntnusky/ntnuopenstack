58795c28b1d13ed87230227925721fbc91aaf27e
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-26 09:07:34 +0200

    Semantics

--- manifests/horizon.pp
+++ manifests/horizon.pp
@@ -81,7 +81,7 @@ class ntnuopenstack::horizon {
     keystone_multidomain_support   => true,
     keystone_url                   => $keystone_url,
     keystone_domain_choices        => [
-      {'name'                      => $ldap_name, 'display' => $description},
+      {'name' => $ldap_name, 'display' => $description},
       {'name' => 'default',  'display' => 'Openstack accounts'},
     ],
     neutron_options                => {
