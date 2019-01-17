e16c37e9cc79555eb4cc2dd788e22a006c229749
Eigil Obrestad <eigil-git@obrestad.org>
2019-01-17 16:26:38 +0100

    Add option to place swift-api at web-ports under its own dedicated name

--- manifests/swift/endpoint.pp
+++ manifests/swift/endpoint.pp
@@ -7,6 +7,32 @@ class ntnuopenstack::swift::endpoint {
   $keystone_password = lookup('ntnuopenstack::swift::keystone::password')
   $region = hiera('ntnuopenstack::region')
 
+  $swiftname = lookup('ntnuopenstack::swift::dns::name', {
+    'default_value' => False,
+  })
+  $certificate = lookup("profile::haproxy::${profile}::webcert", {
+    'default_value' => false,
+  })
+
+  # If no name is set for swift, the service is placed under the regular API
+  # endpoint at port 7480. If name is set swift is placed at port 80/443 under
+  # the supplied name.
+  if($swiftname) {
+    $public = "${endpoint_public}:7480/swift/v1/%(project_id)s"
+    $admin = "${endpoint_admin}:7480/swift/v1"
+    $internal = "${endpoint_internal}:7480/swift/v1/%(project_id)s"
+  } else {
+    if($certificate) {
+      $proto='https'
+    } else {
+      $proto='http'
+    }
+
+    $public = "${proto}://${swiftname}/swift/v1/%(project_id)s"
+    $admin = "${proto}://${swiftname}/swift/v1"
+    $internal = "${proto}://${swiftname}/swift/v1/%(project_id)s"
+  }
+
   keystone::resource::service_identity { 'swift':
     configure_user      => true,
     configure_user_role => true,
@@ -19,8 +45,8 @@ class ntnuopenstack::swift::endpoint {
     password            => $keystone_password,
     email               => 'swift@localhost',
     tenant              => 'services',
-    public_url          => "${endpoint_public}:7480/swift/v1/%(project_id)s",
-    admin_url           => "${endpoint_admin}:7480/swift/v1",
-    internal_url        => "${endpoint_internal}:7480/swift/v1/%(project_id)s",
+    public_url          => $public
+    admin_url           => $admin,
+    internal_url        => $internal, 
   }
 }
