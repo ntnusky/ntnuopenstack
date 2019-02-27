c695f75b2822546611e4ee9b23e2b34a1f89d836
Eigil Obrestad <eigil-git@obrestad.org>
2019-02-27 09:33:54 +0100

    Han der syntax var på besøk. Vemmelig type.

--- manifests/cinder/database.pp
+++ manifests/cinder/database.pp
@@ -1,7 +1,7 @@
 # Sets up the cinder database, and lets cinder populate it 
 class ntnuopenstack::cinder::database {
   $password = lookup('ntnuopenstack::cinder::mysql::password', String)
-  $allowed_hosts = lookup('ntnuopenstack::mysql::allowed_hosts' {
+  $allowed_hosts = lookup('ntnuopenstack::mysql::allowed_hosts', {
     'value_type' => Array[String],
     'merge'      => 'first',
   })
