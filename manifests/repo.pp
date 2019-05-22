423d0f87bf7770e0fb4cf67e9114387a2d87d66e
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-05-22 12:54:17 +0200

    Ensure that it will not silently fail on Debian

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,17 +1,15 @@
 # This class sets up the openstack repositories.
 class ntnuopenstack::repo {
-  if ($::osfamily == 'Debian') {
-    if ($::operatingsystem == 'Ubuntu') {
-      $distro = $facts['os']['release']['major']
-      if ($distro == '18.04' and $::openstack_extras::repo::debian::params::release == 'queens') {
-        notify('Queens is default in Bionic. Do not use UCA')
-      } else {
-        class { '::openstack_extras::repo::debian::ubuntu':
-          package_require => true,
-        }
-      }
+  if ($::osfamily == 'Debian' and $::operatingsystem == 'Ubuntu') {
+    $distro = $facts['os']['release']['major']
+    if ($distro == '18.04' and $::openstack_extras::repo::debian::params::release == 'queens') {
+      notify('Queens is default in Bionic. Do not use UCA')
     } else {
-      fail("Operating system family ${::osfamily} is not supported.")
+      class { '::openstack_extras::repo::debian::ubuntu':
+        package_require => true,
+      }
     }
+  } else {
+    fail("Operating system family ${::osfamily} is not supported.")
   }
 }
