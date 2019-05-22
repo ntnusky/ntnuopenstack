a0a6409cb3da4d8dba1f5ae59f151b0d48320c42
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-05-22 13:46:29 +0200

    To access variable from openstack_extras, include the class it's defined in. Also, notify is not the same as notice

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,9 +1,11 @@
 # This class sets up the openstack repositories.
 class ntnuopenstack::repo {
+  include ::openstack_extras::repo::debian::params
+
   if ($::osfamily == 'Debian' and $::operatingsystem == 'Ubuntu') {
     $distro = $facts['os']['release']['major']
     if ($distro == '18.04' and $::openstack_extras::repo::debian::params::release == 'queens') {
-      notify('Queens is default in Bionic. Do not use UCA')
+      notice('Queens is default in Bionic. Do not use UCA')
     } else {
       class { '::openstack_extras::repo::debian::ubuntu':
         package_require => true,
