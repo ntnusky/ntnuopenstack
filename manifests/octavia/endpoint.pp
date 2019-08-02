9f5e751a80e2957881475f36f295fcd762494451
Eigil Obrestad <eigil-git@obrestad.org>
2019-08-02 15:44:38 +0200

    Register endpoint using the octavia-module's class instead of doing it more manually

--- manifests/octavia/endpoint.pp
+++ manifests/octavia/endpoint.pp
@@ -7,24 +7,15 @@ class ntnuopenstack::octavia::endpoint {
   $keystone_password = lookup('ntnuopenstack::octavia::keystone::password')
   $region = lookup('ntnuopenstack::region')
 
-  $admin = "${endpoint_admin}:9876/"
-  $public = "${endpoint_public}:9876/"
-  $internal = "${endpoint_internal}:9876/"
+  $admin = "${endpoint_admin}:9876"
+  $public = "${endpoint_public}:9876"
+  $internal = "${endpoint_internal}:9876"
 
-  keystone::resource::service_identity { 'octavia':
-    configure_user      => true,
-    configure_user_role => true,
-    configure_endpoint  => true,
-    service_type        => 'load-balancer',
-    service_description => 'Openstack Octavia LBaaS service',
-    service_name        => 'octavia',
-    auth_name           => 'octavia',
-    region              => $region,
-    password            => $keystone_password,
-    email               => 'octavia@localhost',
-    tenant              => 'services',
-    public_url          => $public,
-    admin_url           => $admin,
-    internal_url        => $internal,
+  class { 'octavia::keystone::auth':
+    password     => $keystone_password,
+    region       => $region,
+    public_url   => $public,
+    admin_url    => $admin,
+    internal_url => $internal,
   }
 }
