c6175b9e5f4bfce54c814f56ba8a9b137169323a
Eigil Obrestad <eigil-git@obrestad.org>
2019-08-02 14:25:00 +0200

    Uhm. I do not how I planned that to work.

--- manifests/octavia.pp
+++ manifests/octavia.pp
@@ -24,12 +24,12 @@ class ntnuopenstack::octavia {
 
   include ::ntnuopenstack::octavia::base
 
-  ::octavia::api {
+  class { '::octavia::api':
     port    => $api_port,
     sync_db => $dbsync,
   }
 
-  ::octavia::controller {
+  class { '::octavia::controller':
     amp_flavor_id         => $flavor_id,
     amp_image_tag         => $image_tag,
     amp_secgroup_list     => [$secgroup_id],
@@ -38,31 +38,31 @@ class ntnuopenstack::octavia {
     amp_ssh_key_name      => $keypair,
   }
 
-  ::octavia::health_manager {
+  class { '::octavia::health_manager':
     heartbeat_key => $heartbeat_key,
   }
 
-  ::octavia::housekeeping {
+  class { '::octavia::housekeeping':
     spare_amphorae_pool_size => $spare_pool_size,
   }
 
-  ::octavia::certificates {
-    cert_generator => local_cert_generator,
+  class { '::octavia::certificates':
+    cert_generator              => local_cert_generator,
 
     # The CA certificate used to sign the amphora certs
-    ca_certificate => '/etc/octavia/certs/server_ca.cert.pem',
-    ca_certificate_data => $server_ca_cert,
-    ca_private_key => '/etc/octavia/certs/server_ca.key.pem',
-    ca_private_key_data => $server_ca_key,
-    ca_private_key_passphrase => $server_ca_passphrase,
+    ca_certificate              => '/etc/octavia/certs/server_ca.cert.pem',
+    ca_certificate_data         => $server_ca_cert,
+    ca_private_key              => '/etc/octavia/certs/server_ca.key.pem',
+    ca_private_key_data         => $server_ca_key,
+    ca_private_key_passphrase   => $server_ca_passphrase,
     server_certs_key_passphrase => $amphora_certs_keyphrase,
 
     # The CA certificate used to sign the octavia-controller's certificates.
-    client_ca => '/etc/octavia/certs/client_ca.cert.pem',
-    client_ca_data => $client_ca_cert,
+    client_ca                   => '/etc/octavia/certs/client_ca.cert.pem',
+    client_ca_data              => $client_ca_cert,
 
     # The certificate for the octavia-controller.
-    client_cert => '/etc/octavia/certs/client.cert.pem',
-    client_cert_data => $client_cert,
+    client_cert                 => '/etc/octavia/certs/client.cert.pem',
+    client_cert_data            => $client_cert,
   }
 }
