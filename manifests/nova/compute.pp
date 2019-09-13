6d4226b15726ddb93f58262c1086ef0ae7bfc5cc
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-09-13 09:00:24 +0200

    One day I will probably stop making spelling mistakes...

--- manifests/nova/compute.pp
+++ manifests/nova/compute.pp
@@ -56,7 +56,7 @@ class ntnuopenstack::nova::compute {
   if ($install_sensu) {
     sensu::subscription { 'os-compute': }
   }
-  ensure_package( ['python3-pymysql'] , {
+  ensure_packages( ['python3-pymysql'] , {
     'ensure' => 'present',
   })
 }
