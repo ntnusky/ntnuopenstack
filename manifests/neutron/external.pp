22f99fc6ac2862bb17c2edf3ed852f3ff8ecb2d8
Eigil Obrestad <eigil-git@obrestad.org>
2019-10-25 13:27:30 +0200

    Add the possibility to have external networks on a vswitch VLAN.
    Updates hiera-format for the key 'ntnuopenstack::neutron::external::connections'.
    
    Previously the following format was used:
    ```
    ntnuopenstack::neutron::external::connections:
      ntnuint: 'ens1f0'
    ```
    
    Now the following format should be used for the same thing:
    ```
    ntnuopenstack::neutron::external::connections:
      ntnuint:
        type: 'interface'
        interface: 'ens1f0'
    ```
    
    If the network should be connected to an existing vswitch the following can be set in hiera:
    ```
    ntnuopenstack::neutron::external::connections:
      ntnuint:
        type: 'vswitch'
        bridge: 'infra'
        vlan: 1337
    ```

--- manifests/neutron/external.pp
+++ manifests/neutron/external.pp
@@ -5,31 +5,60 @@ class ntnuopenstack::neutron::external {
     'value_type' => Hash[String, Hash],
   })
   $connections = lookup('ntnuopenstack::neutron::external::connections', {
-    'value_type'    => Hash[String, String],
+    'value_type'    => Hash[String, Variant[String, Hash]],
     'default_value' => {},
   })
 
   require ::vswitch::ovs
 
   # For each connection to an external network this l3-node should create:
-  $connections.each | $netname, $interface | {
-    # If a VLAN-ID is set there is a need for a virtual patch between the ovs
-    # bridge connected to the physical interface and the ovs bridge representing
-    # the virtual network.
-    if('vlanid' in $externalnets[$netname]) {
-      $n = "${interface}-${externalnets[$netname]['vlanid']}-br-${netname}"
-      ::profile::infrastructure::ovs::patch { $n :
-        physical_if => $interface,
-        vlan_id     => $externalnets[$netname]['vlanid'],
-        ovs_bridge  => "br-${netname}",
+  $connections.each | $netname, $data | {
+    # If the data is a hash, its the new-style configuration.
+    if($data =~ Hash) {
+      # A 'vswitch' connection would indicate that an external network is
+      # present at a certain VLAN on a certain vswitch bridge.
+      if($data['type'] == 'vswitch') {
+        $n = "Connection between ${data['bridge']} and br-${netname}"
+        ::profile::infrastructure::ovs::patch::vlan { $n:
+          source_bridge      => $data['bridge'],
+          source_vlan        => $data['vlan'],
+          destination_bridge => "br-${netname}",
+        }
+
+      # An 'interface' connection would mean that a physical interface is
+      # dedicated to a certain external network.
+      } elsif ($data['type'] == 'interface') {
+        vs_port { $data['interface']:
+          ensure => present,
+          bridge => "br-${netname}",
+        }
+
+      # It an unknown type is defined; fail with an error.
+      } else {
+        fail("${data['type']} is an invalid type for an external network")
       }
 
-    # If there is not a VLAN ID set the bridge representing the virtual network
-    # can simply be connected to the physical interface
-    } else {
-      vs_port { $interface:
-        ensure => present,
-        bridge => "br-${netname}",
+    # If the data is a string, we use 'old-style' syntax, and in that case data
+    # should be an interface name.
+    } elsif($data =~ String) {
+      # If a VLAN-ID is set there is a need for a virtual patch between the ovs
+      # bridge connected to the physical interface and the ovs bridge representing
+      # the virtual network.
+      if('vlanid' in $externalnets[$netname]) {
+        $n = "${data}-${externalnets[$netname]['vlanid']}-br-${netname}"
+        ::profile::infrastructure::ovs::patch { $n :
+          physical_if => $data,
+          vlan_id     => $externalnets[$netname]['vlanid'],
+          ovs_bridge  => "br-${netname}",
+        }
+
+      # If there is not a VLAN ID set the bridge representing the virtual network
+      # can simply be connected to the physical interface
+      } else {
+        vs_port { $data:
+          ensure => present,
+          bridge => "br-${netname}",
+        }
       }
     }
   }
