6288c6c721eeee97707f21ed08b834a762d91435
Eigil Obrestad <eigil-git@obrestad.org>
2019-10-25 09:38:31 +0200

    Allow tenant-VLAN's to connect to a vswitch

--- manifests/neutron/tenant/vlan.pp
+++ manifests/neutron/tenant/vlan.pp
@@ -7,18 +7,31 @@ class ntnuopenstack::neutron::tenant::vlan {
   include ::ntnuopenstack::neutron::ml2::config
   require ::vswitch::ovs
 
+  # If the tenant-vlans are configured to be present on a VLAN, print an error.
   if($tenant_if == 'vlan') {
     $a = 'It is impossible to use a VLAN for tenant_if when using VLANs to'
     $b = 'separate tenant networks.'
     fail("${a} ${b}")
-  }
 
-  class { '::ntnuopenstack::neutron::ovs':
-    tenant_mapping => 'physnet-vlan:br-vlan',
-  }
+  # If the tenant-vlans are connected to a vswitch which already exists, connect
+  # to that vswitch.
+  } elsif ($tenant_if == 'vswitch') {
+    $tenant_bridge = lookup('ntnuopenstack::tenant::bridge')
 
-  vs_port { $tenant_if:
-    ensure => present,
-    bridge => 'br-vlan',
+    class { '::ntnuopenstack::neutron::ovs':
+      tenant_mapping => "physnet-vlan:${tenant_bridge}",
+    }
+
+  # If the tenant-VLANS are connected to a physical if; create the bridge
+  # 'br-vlan' and add the physical interface to it.
+  } else {
+    class { '::ntnuopenstack::neutron::ovs':
+      tenant_mapping => 'physnet-vlan:br-vlan',
+    }
+
+    vs_port { $tenant_if:
+      ensure => present,
+      bridge => 'br-vlan',
+    }
   }
 }
