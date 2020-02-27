9af11063b8b020895165dd20761eb193a6d75aa9
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-02-27 13:54:29 +0100

    Add barbican-client

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -2,10 +2,19 @@
 class ntnuopenstack::clients {
   require ::ntnuopenstack::repo
 
+  $barbican = lookup('ntnuopenstack::barbican::keystone::password', {
+    'default_value' => false,
+    'value_type'    => Variant[Boolean, String],
+  })
+
   include ::keystone::client
   include ::cinder::client
   include ::nova::client
   include ::neutron::client
   include ::glance::client
   include ::heat::client
+
+  if($barbican) {
+    include ::barbican::client
+  }
 }
