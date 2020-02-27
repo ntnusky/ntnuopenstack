295a61f96bee18b82f35f87b9f9d2c28ced03004
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-02-27 12:57:10 +0100

    Wrong parameter name for simple_crypto kek

--- manifests/barbican/crypto.pp
+++ manifests/barbican/crypto.pp
@@ -1,9 +1,9 @@
 # Configure crypto plugin for barbican
 class ntnuopenstack::barbican::crypto {
-  $simple_crypto_kek = lookup('ntnuopenstack::barbican::simple_crypto::kek', Stdlib::Base64)
+  $kek = lookup('ntnuopenstack::barbican::simple_crypto::kek', Stdlib::Base64)
 
   class { '::barbican::plugins::simple_crypto':
-    simple_crypto_kek => $simple_crypto_kek,
+    simple_crypto_plugin_kek => $kek,
     global_default    => true,
   }
 }
