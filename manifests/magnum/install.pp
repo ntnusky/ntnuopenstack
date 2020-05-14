48d380285089aaebb3f53e38d63ecc8c3406c984
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-05-14 15:23:46 +0200

    Create systemd-service for magnum-conductor using files extracted from deb packages

--- manifests/magnum/install.pp
+++ manifests/magnum/install.pp
@@ -1,4 +1,6 @@
 # Install magnum with pip3
+# And hack together all binaries and services
+# They are stolen from the deb-packages
 class ntnuopenstack::magnum::install {
   package { 'magnum':
     ensure          => '8.2.0',
@@ -10,5 +12,20 @@ class ntnuopenstack::magnum::install {
   file { '/etc/magnum':
     ensure  => 'directory',
     require => Package['magnum'],
+  }
+
+  file { '/etc/init.d/magnum-conductor':
+    ensure => 'file',
+    source => 'puppet:///modules/ntnuopenstack/openstack/magnum/services/magnum-conductor',
+  }
+
+  file { '/usr/bin':
+    ensure  => 'directory',
+    recurse => remote,
+    source  => 'puppet:///modules/ntnuopenstack/openstack/magnum/bin',
+  }
+
+  systemd::unit_file { 'magnum-conductor.service':
+    source => 'puppet:///modules/ntnuopenstack/openstack/magnum/services/magnum-conductor.service',
   }
 }
