dd8beff672eecb680e2cc07e95047580cb628359
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-05-14 13:49:19 +0200

    Tell pip3 where to install

--- manifests/magnum/install.pp
+++ manifests/magnum/install.pp
@@ -1,13 +1,16 @@
 # Install magnum with pip3
 class ntnuopenstack::magnum::install {
   package { 'magnum':
-    ensure   => '8.2.0',
-    provider => 'pip3',
-    tag      => ['openstack', 'magnum-package'],
+    ensure          => '8.2.0',
+    provider        => 'pip3',
+    install_options => {
+      '-t' => '/usr/lib/python3/dist-packages',
+    },
+    tag             => ['openstack', 'magnum-package'],
   }
 
   file { '/etc/magnum':
-    ensure   => 'directory',
-    require  => Package['magnum'],
+    ensure  => 'directory',
+    require => Package['magnum'],
   }
 }
