667c010076aa140bb0d82c0f7e335684fe290ade
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-06-17 12:56:30 +0200

    Add support for vGPUs in nova

---
+++ manifests/nova/vgpu.pp
@@ -0,0 +1,10 @@
+# Configure nova-compute to expose a vGPU
+class ntnuopenstack::nova::vgpu {
+
+  $vgpu_type = lookup('ntnuopenstack::nova::vgpu::type', String)
+
+  class { '::nova::compute::vgpu':
+    enabled_vgpu_types => $vgpu_type,
+  }
+
+}
