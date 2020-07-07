66d48b6781fc5edcdfccf1c23a0a9d22bb79fb14
Eigil Obrestad <eigil-git@obrestad.org>
2020-07-07 15:47:38 +0200

    Use correct names

--- manifests/placement/firewall/haproxy/internal.pp
+++ manifests/placement/firewall/haproxy/internal.pp
@@ -1,6 +1,6 @@
 # Configures the firewall to accept incoming traffic to the placement API. 
 class ntnuopenstack::placement::firewall::haproxy::internal {
-  ::profile::baseconfig::firewall::service::internal { 'Placement':
+  ::profile::baseconfig::firewall::service::infra { 'Placement':
     protocol => 'tcp',
     port     => 8778,
   }
