86037225d927037b63c1714af6ba8aaba30fced5
Eigil Obrestad <eigil-git@obrestad.org>
2020-07-08 11:55:47 +0200

    Configure the database connection

--- manifests/placement/api.pp
+++ manifests/placement/api.pp
@@ -1,23 +1,21 @@
 # Creates the databases for the placement service.
 class ntnuopenstack::placement::api {
-  $confhaproxy = lookup('ntnuopenstack::haproxy::configure::backend', {
-    'value_type'    => Boolean,
-    'default_value' => true,
-  })
+  # Determine database-settings
+  $mysql_pass = lookup('ntnuopenstack::placement::mysql::password', String)
+  $mysql_ip = lookup('ntnuopenstack::placement::mysql::ip', Stdlib::IP::Address)
+  $database_connection = "mysql+pymysql://placement:${mysql_pass}@${mysql_ip}/placement"
   $db_sync = lookup('ntnuopenstack::placement::db::sync̈́', Boolean)
 
+  # Retrieve openstack-settings
   $region = lookup('ntnuopenstack::region', String)
   $keystone_password = lookup('ntnuopenstack::placement::keystone::password',
                                 String)
-
-  # Determine where the keystone service is located.
   $keystone_internal = lookup('ntnuopenstack::keystone::endpoint::internal',
                                 Stdlib::Httpurl)
   $keystone_public = lookup('ntnuopenstack::keystone::endpoint::public',
                                 Stdlib::Httpurl)
 
-  # Retrieve addresses for the memcached servers, either the old IP or the new
-  # list of hosts.
+  # Retrieve addresses for the memcached servers
   $cache_servers = lookup('profile::memcache::servers', {
     'value_type'    => Array[String],
     'merge'         => 'unique',
@@ -26,10 +24,14 @@ class ntnuopenstack::placement::api {
     "${server}:11211"
   }
 
+  # Determining if the server is placed behind haproxy
+  $confhaproxy = lookup('ntnuopenstack::haproxy::configure::backend', {
+    'value_type'    => Boolean,
+    'default_value' => true,
+  })
+
   require ::ntnuopenstack::repo
   include ::ntnuopenstack::placement::firewall::server
-  include ::ntnuopenstack::placement::haproxy::backend
-
 
   if($confhaproxy) {
     contain ::ntnuopenstack::placement::haproxy::backend
@@ -40,6 +42,10 @@ class ntnuopenstack::placement::api {
 
   class { '::placement':
     sync_db => $db_sync,
+  }
+
+  class { '::placement::db':
+    database_connection => $database_connection,
   }
 
   class { '::placement::keystone::authtoken':
