c5c73add143ed55d0884e1a77a3688b4c4b6fd56
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-08-13 13:29:31 +0200

    Enable CORS for glance

--- manifests/glance/api.pp
+++ manifests/glance/api.pp
@@ -30,6 +30,13 @@ class ntnuopenstack::glance::api {
     'default_value' => true,
   })
 
+  $horizon_fqdn = lookup('ntnuopenstack::horizon::server_name')
+  $horizon_url = "https://${horizon_fqdn}/horizon"
+  $upload_mode = lookup('ntnuopenstack::horizon::upload_mode', {
+    'value_type'   => Enum['legacy', 'direct', 'off'],
+    'default_value' => 'legacy',
+  })
+
   require ::ntnuopenstack::repo
   contain ::ntnuopenstack::glance::ceph
   contain ::ntnuopenstack::glance::firewall::server::api
@@ -67,5 +74,11 @@ class ntnuopenstack::glance::api {
 
   glance_api_config {
     'DEFAULT/default_store': value => 'rbd';
+  }
+
+  if ($upload_mode == 'direct') {
+    glance_api_config {
+      'cors/allowed_origin': value => $horizon_url;
+    }
   }
 }
