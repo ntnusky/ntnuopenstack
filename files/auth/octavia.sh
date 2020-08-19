96a3eeda9288996a88dabac2e0630af848c63946
Eigil Obrestad <eigil-git@obrestad.org>
2020-08-19 14:17:08 +0200

    Add template for octavia openrc file

---
+++ files/auth/octavia.sh
@@ -0,0 +1,8 @@
+unset OS_SERVICE_TOKEN
+export OS_USERNAME=octavia
+export OS_PASSWORD='octavia'
+export OS_AUTH_URL=http://KEYSTONE_IP:5000/v3
+export OS_PROJECT_NAME=services
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_IDENTITY_API_VERSION=3
