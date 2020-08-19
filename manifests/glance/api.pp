59941f08268d644528bee0ac9e55732ed35480c8
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-08-19 12:25:22 +0200

    Let the disk_formats in hiera be a hash, so it can be used with horizon as well, for adding a description to each type

--- manifests/glance/api.pp
+++ manifests/glance/api.pp
@@ -38,15 +38,14 @@ class ntnuopenstack::glance::api {
   })
 
   $disk_formats = lookup('ntnuopenstack::glance::disk_formats', {
-    'value_type'    => Array[String],
-    'default_value' => ['raw', 'qcow2'],
+    'default_value' =>  ['raw', 'qcow2']
   })
   $container_formats = lookup('ntnuopenstack::glance::container_formats', {
     'value_type'    => Array[String],
     'default_value' => ['bare'],
   })
 
-  $disk_formats_real = join($disk_formats, ',')
+  $disk_formats_real = join(keys($disk_formats), ',')
   $container_formats_real = join($container_formats, ',')
 
   require ::ntnuopenstack::repo
