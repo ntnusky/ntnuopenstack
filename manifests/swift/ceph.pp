9502e1b9b48098262410be9eef51e4f65d4d4b1f
Eigil Obrestad <eigil-git@obrestad.org>
2020-11-04 14:00:55 +0100

    Add keys for new radosgw-names

--- manifests/swift/ceph.pp
+++ manifests/swift/ceph.pp
@@ -2,13 +2,15 @@
 class ntnuopenstack::swift::ceph {
   $cephx_key = lookup('ntnuopenstack::swift::ceph::key', String)
 
+  $hostname = $trusted['hostname']
+
   require ::profile::ceph::client
 
   ceph_config {
-      'client.radosgw.main/key': value => $cephx_key;
+      "client.radosgw.${hostname}/key": value => $cephx_key;
   }
 
-  ceph::key { 'client.radosgw.main':
+  ceph::key { "client.radosgw.${hostname}":
     secret  => $cephx_key,
     cap_mon => 'allow rwx',
     cap_osd => 'allow rwx',
