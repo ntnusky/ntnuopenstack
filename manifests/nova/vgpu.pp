978bb7975834d247c68e1378a7a30a9316fe1e04
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-12-01 12:30:55 +0100

    Change this dummy-check to use the new hiera key for defining vgpus

--- manifests/nova/vgpu.pp
+++ manifests/nova/vgpu.pp
@@ -2,5 +2,5 @@
 # This is just a mechanism to ensure that this key is actually defined,
 # and otherwise fail the puppet run.
 class ntnuopenstack::nova::vgpu {
-  $vgpu_type = lookup('nova::compute::vgpu::enabled_vgpu_types', String)
+  $vgpu_type = lookup('nova::compute::vgpu::vgpu_types_device_addresses_mapping', Hash)
 }
