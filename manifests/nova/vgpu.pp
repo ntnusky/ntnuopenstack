e329f316b169f9c153135a486ebd29dfd90b7600
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-12-03 09:33:24 +0100

    Move the comments where they belong

--- manifests/nova/vgpu.pp
+++ manifests/nova/vgpu.pp
@@ -1,12 +1,11 @@
 # Configure nova-compute to expose a vGPU
-# This is just a mechanism to ensure that this key is actually defined,
-# and otherwise fail the puppet run.
 class ntnuopenstack::nova::vgpu {
+  # This is just a mechanism to ensure that this key is actually defined,
+  # and otherwise fail the puppet run.
   $vgpu_type = lookup('nova::compute::vgpu::vgpu_types_device_addresses_mapping', Hash)
 
   # Ensure SR-IOV virtual functions are enabled on boot
   # This has no effect on GPUs that don't support SR-IOV
-
   cron { 'Enable SR-IOV for Nvidia GPUs on reboot':
     command => '/usr/lib/nvidia/sriov-manage -e ALL',
     user    => 'root',
