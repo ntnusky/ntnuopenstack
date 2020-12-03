be3da67e04f58b2231d38fad9fe253e50e853c38
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-12-03 09:20:32 +0100

    Enable SR-IOV on boot. Add a ninja-patch needed in sriov-manage before nova bug #1906494 is fixed upstream

--- manifests/nova/vgpu.pp
+++ manifests/nova/vgpu.pp
@@ -3,4 +3,21 @@
 # and otherwise fail the puppet run.
 class ntnuopenstack::nova::vgpu {
   $vgpu_type = lookup('nova::compute::vgpu::vgpu_types_device_addresses_mapping', Hash)
+
+  # Ensure SR-IOV virtual functions are enabled on boot
+  # This has no effect on GPUs that don't support SR-IOV
+
+  cron { 'Enable SR-IOV for Nvidia GPUs on reboot':
+    command => '/usr/lib/nvidia/sriov-manage -e ALL',
+    user    => 'root',
+    special => 'reboot',
+  }
+
+  # This can be removed when nova bug #1906494 is fixed
+  file_line { 'sriov-manage':
+    ensure => present,
+    path   => '/usr/lib/nvidia/sriov-manage',
+    line   => 'local numvfs=2',
+    match  => 'local numvfs=\$2',
+  }
 }
