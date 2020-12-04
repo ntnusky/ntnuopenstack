6a8f29392c8f15a1509ba75c53270e5bedb827b6
Eigil Obrestad <eigil-git@obrestad.org>
2020-12-04 15:56:10 +0100

    Add monitoring of VGPU's

--- manifests/nova/vgpu.pp
+++ manifests/nova/vgpu.pp
@@ -1,5 +1,7 @@
 # Configure nova-compute to expose a vGPU
 class ntnuopenstack::nova::vgpu {
+  include ::profile::monitoring::munin::plugin::vgpu
+
   # This is just a mechanism to ensure that this key is actually defined,
   # and otherwise fail the puppet run.
   $vgpu_type = lookup('nova::compute::vgpu::vgpu_types_device_addresses_mapping', Hash)
