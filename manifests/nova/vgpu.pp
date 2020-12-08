08040cec39d15aba9a6fc755e34feee8f9536b1b
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-12-08 15:06:41 +0100

    Make it possible how many SR-IOV VFs you need. Default: do not hack the script

--- manifests/nova/vgpu.pp
+++ manifests/nova/vgpu.pp
@@ -3,6 +3,10 @@ class ntnuopenstack::nova::vgpu {
   # This is just a mechanism to ensure that this key is actually defined,
   # and otherwise fail the puppet run.
   $vgpu_type = lookup('nova::compute::vgpu::vgpu_types_device_addresses_mapping', Hash)
+  $numvfs = lookup('ntnuopenstack::nova::vgpu::numvfs', {
+    'value_type'    => Variant[Boolean, Integer],
+    'default_value' => false,
+  })
 
   # Ensure SR-IOV virtual functions are enabled on boot
   # This has no effect on GPUs that don't support SR-IOV
@@ -17,12 +21,14 @@ class ntnuopenstack::nova::vgpu {
   }
 
   # This can be removed when nova bug #1906494 is fixed
-  file_line { 'sriov-manage':
-    ensure => present,
-    path   => '/usr/lib/nvidia/sriov-manage',
-    line   => 'local numvfs=2',
-    match  => 'local numvfs=\$2',
-    notify => Exec['sriov-manage-firstrun'],
+  if ($numvfs) {
+    file_line { 'sriov-manage':
+      ensure => present,
+      path   => '/usr/lib/nvidia/sriov-manage',
+      line   => "local numvfs=${numvfs}",
+      match  => 'local numvfs=\$2',
+      notify => Exec['sriov-manage-firstrun'],
+    }
   }
 
   exec { 'sriov-manage-firstrun':
