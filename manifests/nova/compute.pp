f8cd5ce8757bbe98c33ad648f4c1d490e4ee3dc8
Eigil Obrestad <eigil-git@obrestad.org>
2022-01-20 14:00:03 +0100

    Splitting the nova-compute service out from ntnuopenstack::nova::compute

--- manifests/nova/compute.pp
+++ manifests/nova/compute.pp
@@ -1,49 +1,18 @@
 # This class installs and configures nova for a compute-node.
 class ntnuopenstack::nova::compute {
-  # Determine the VNCProxy-settings
-  $host = lookup('ntnuopenstack::nova::vncproxy::host')
-  $port = lookup('ntnuopenstack::nova::vncproxy::port', {
-    'default_value' => 6080,
-    'value_type'    => Integer,
-  })
-  $cert = lookup('ntnuopenstack::endpoint::public::cert', {
-    'default_value' => false,
-  })
-
-  $management_if = lookup('profile::interfaces::management')
-  $management_ip = getvar("::ipaddress_${management_if}")
-
   require ::ntnuopenstack::nova::compute::base
   contain ::ntnuopenstack::nova::compute::libvirt
+  include ::ntnuopenstack::nova::compute::service
   contain ::ntnuopenstack::nova::common::neutron
   include ::ntnuopenstack::nova::munin::compute
   require ::ntnuopenstack::repo
-
-  if($cert) {
-    $protocol = 'https'
-  } else {
-    $protocol = 'http'
-  }
-
-  class { '::nova::compute':
-    enabled                          => true,
-    vnc_enabled                      => true,
-    vncserver_proxyclient_address    => $management_ip,
-    vncproxy_host                    => $host,
-    vncproxy_protocol                => $protocol,
-    vncproxy_port                    => $port,
-    resume_guests_state_on_host_boot => true,
-  }
 
   class { '::ntnuopenstack::nova::compute::ceph':
     ephemeral_storage => true,
   }
 
-  user { 'nova':
-    shell => '/bin/bash',
-    home  => '/var/lib/nova',
-  }
-
+  # Determine if sensu should be installed, and in that case include a sensu
+  # subscription.
   $install_sensu = lookup('profile::sensu::install', {
     'default_value' => true,
     'value_type'    => Boolean,
@@ -51,15 +20,4 @@ class ntnuopenstack::nova::compute {
   if ($install_sensu) {
     sensu::subscription { 'os-compute': }
   }
-
-  $pymysql_pkg = $::osfamily ? {
-    'RedHat' => 'python2-PyMySQL',
-    'Debian' => 'python3-pymysql',
-    default  => '',
-  }
-
-  ensure_packages( [$pymysql_pkg] , {
-    'ensure' => 'present',
-  })
 }
-
