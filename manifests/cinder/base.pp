67e1e688b5776fdf7f16f97520f08b697dc8b3c5
Eigil Obrestad <eigil-git@obrestad.org>
2022-05-06 11:54:20 +0200

    Use the new cinder::db class

--- manifests/cinder/base.pp
+++ manifests/cinder/base.pp
@@ -1,10 +1,5 @@
 # Configures the base cinder config
 class ntnuopenstack::cinder::base {
-  # Determine where the database resides 
-  $mysql_pass = lookup('ntnuopenstack::cinder::mysql::password', String)
-  $mysql_ip = lookup('ntnuopenstack::cinder::mysql::ip', Stdlib::IP::Address)
-  $database_connection = "mysql+pymysql://cinder:${mysql_pass}@${mysql_ip}/cinder"
-
   # Credentials for the messagequeue
   $transport_url = lookup('ntnuopenstack::transport::url', String)
   $rabbitservers = lookup('profile::rabbitmq::servers', {
@@ -27,9 +22,9 @@ class ntnuopenstack::cinder::base {
 
   require ::ntnuopenstack::repo
   require ::ntnuopenstack::cinder::sudo
+  include ::ntnuopenstack::dbconnection
 
   class { '::cinder':
-    database_connection   => $database_connection,
     default_transport_url => $transport_url,
     *                     => $ha_transport_conf,
   }
