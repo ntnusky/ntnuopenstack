b498514a22101d80f5b8a8603d442ca79d138aaf
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2022-05-09 10:52:27 +0200

    Enable angular panel for images

--- manifests/horizon/base.pp
+++ manifests/horizon/base.pp
@@ -91,6 +91,7 @@ class ntnuopenstack::horizon::base {
       {'name' => 'default',  'display' => 'Openstack accounts'},
     ],
     image_backend                  => $image_backend,
+    images_panel                   => 'angular'
     manage_memcache_package        => false,
     instance_options               => {
       create_volume => false,
