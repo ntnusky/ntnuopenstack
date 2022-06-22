39e669081452474bec0bbd353025adc9d1480a19
Eigil Obrestad <eigil-git@obrestad.org>
2022-06-22 11:00:39 +0200

    Hash != Array.

--- manifests/nova/compute/provider.pp
+++ manifests/nova/compute/provider.pp
@@ -1,8 +1,8 @@
 # Configures the compute-nodes providers 
 class ntnuopenstack::nova::compute::provider {
   $providers = lookup('ntnuopenstack::nova::compute::providers', {
-    'default_value' => {},
-    'value_type'    => Hash[String,Hash], 
+    'default_value' => []
+    'value_type'    => Array[Hash], 
   })
 
   class {'::nova::compute::provider': 
