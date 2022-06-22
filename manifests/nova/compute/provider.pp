cadb08a61b4b932899e6e9f286bfcb9150c30bb8
Eigil Obrestad <eigil-git@obrestad.org>
2022-06-22 11:02:19 +0200

    Syntax...

--- manifests/nova/compute/provider.pp
+++ manifests/nova/compute/provider.pp
@@ -1,7 +1,7 @@
 # Configures the compute-nodes providers 
 class ntnuopenstack::nova::compute::provider {
   $providers = lookup('ntnuopenstack::nova::compute::providers', {
-    'default_value' => []
+    'default_value' => [],
     'value_type'    => Array[Hash], 
   })
 
