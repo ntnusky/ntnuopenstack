4e375d205839b5429695bd09812c6bd512a2c7be
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2023-02-13 09:41:00 +0100

    Adopt the new woke command names, intoduced in Pacific

--- manifests/glance/ceph.pp
+++ manifests/glance/ceph.pp
@@ -10,7 +10,7 @@ class ntnuopenstack::glance::ceph {
 
   ceph::key { 'client.glance':
     secret  => $glance_key,
-    cap_mon => 'allow r, allow command "osd blacklist"',
+    cap_mon => 'allow r, allow command "osd blocklist"',
     cap_osd =>
       'allow class-read object_prefix rbd_children, allow rwx pool=images',
     inject  => true,
