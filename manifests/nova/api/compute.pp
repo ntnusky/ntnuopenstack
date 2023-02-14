ad03961bf1cdd472994b7326ef5ddd31490b2fd0
Eigil Obrestad <eigil-git@obrestad.org>
2023-02-14 13:10:20 +0100

    Does it help with ordering to keep the auth in its own class?

--- manifests/nova/api/compute.pp
+++ manifests/nova/api/compute.pp
@@ -1,31 +1,12 @@
 # Installs and configures the nova compute API.
 class ntnuopenstack::nova::api::compute {
-  # Retrieve addresses for the memcached servers
-  $memcache_servers = lookup('profile::memcache::servers', {
-    'value_type'    => Array[Stdlib::IP::Address],
-    'default_value' => [],
-  })
-  $memcache = $memcache_servers.map | $server | {
-    "${server}:11211"
-  }
-
-  # Retrieve openstack parameters
-  $nova_password = lookup('ntnuopenstack::nova::keystone::password')
   $sync_db = lookup('ntnuopenstack::nova::db::sync', {
     'value_type'    => Boolean,
     'default_value' => false,   # One of your nodes need to have this key set to
                                 # true to automaticly populate the databases.
   })
-  $region = lookup('ntnuopenstack::region')
 
-  $use_keystone_limits = lookup('ntnuopenstack::glance::keystone::limits', {
-    'default_value' => false,
-    'value_type'    => Boolean,
-  })
-
-  $internal_endpoint = lookup('ntnuopenstack::endpoint::internal')
-  $public_endpoint = lookup('ntnuopenstack::endpoint::public')
-
+  require ::ntnuopenstack::nova::auth
   include ::ntnuopenstack::nova::common::neutron
   contain ::ntnuopenstack::nova::firewall::server
   contain ::ntnuopenstack::nova::haproxy::backend::api
@@ -33,14 +14,6 @@ class ntnuopenstack::nova::api::compute {
   include ::ntnuopenstack::nova::quota
   require ::ntnuopenstack::nova::services::base
   require ::ntnuopenstack::repo
-
-  class { '::nova::keystone::authtoken':
-    auth_url             => "${internal_endpoint}:5000/",
-    memcached_servers    => $memcache,
-    password             => $nova_password,
-    region_name          => $region,
-    www_authenticate_uri => "${public_endpoint}:5000/",
-  }
 
   class { '::nova::api':
     enabled                      => false,
@@ -52,16 +25,8 @@ class ntnuopenstack::nova::api::compute {
     use_forwarded_for            => true,
   }
 
-  class { '::nova::keystone':
-    auth_url    => "${internal_endpoint}:5000/",
-    password    => $nova_password,
-    region_name => $region,
-    username    => 'nova',
-  }
-
   class { '::nova::wsgi::apache_api':
     ssl               => false,
     access_log_format => 'forwarded',
   }
-
 }
