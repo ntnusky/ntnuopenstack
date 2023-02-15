c1bba205854ceb13438a16b314c13b8044600389
Eigil Obrestad <eigil-git@obrestad.org>
2023-02-15 15:10:41 +0100

    DB-Sync is now configured at ::heat::db instead of ::heat

--- manifests/heat/dbconnection.pp
+++ manifests/heat/dbconnection.pp
@@ -4,8 +4,10 @@ class ntnuopenstack::heat::dbconnection {
   $mysql_pass = lookup('ntnuopenstack::heat::mysql::password', String)
   $mysql_ip = lookup('ntnuopenstack::heat::mysql::ip', Stdlib::IP::Address)
   $database_connection = "mysql+pymysql://heat:${mysql_pass}@${mysql_ip}/heat"
+  $db_sync = lookup('ntnuopenstack::heat::db::sync', Boolean)
 
   class { '::heat::db':
-    database_connection          => $database_connection,
+    database_connection => $database_connection,
+    sync_db             => $db_sync,
   }
 }
