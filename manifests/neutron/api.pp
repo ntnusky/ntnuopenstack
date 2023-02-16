d75311b9174029cf04c639f96ff22b204f014fe7
Eigil Obrestad <eigil-git@obrestad.org>
2023-02-16 11:21:02 +0100

    Revert "the attempt to use the common authtoken classes for now"
    This reverts commit f82adb60e55db4a8712a9e5dfe7ab04c6326098b.

--- manifests/neutron/api.pp
+++ manifests/neutron/api.pp
@@ -20,8 +20,6 @@ class ntnuopenstack::neutron::api {
   include ::ntnuopenstack::neutron::ml2::config
   include ::profile::monitoring::munin::plugin::openstack::neutronapi
 
-  Ntnuopenstack::Common::Authtoken -> Class['neutron::server']
-
   # Install the neutron api
   class { '::neutron::server':
     allow_automatic_l3agent_failover => true,
@@ -29,5 +27,6 @@ class ntnuopenstack::neutron::api {
     enable_proxy_headers_parsing     => $register_loadbalancer,
     service_providers                => $service_providers,
     sync_db                          => $sync_db,
+    require                          => Class['neutron::keystone::authtoken'],
   }
 }
