a7e0229cce79b2abc401df63e47b89b4c7824170
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-24 14:45:22 +0100

    Place neutron-api behind WSGI

--- manifests/neutron/api.pp
+++ manifests/neutron/api.pp
@@ -24,8 +24,21 @@ class ntnuopenstack::neutron::api {
   class { '::neutron::server':
     allow_automatic_l3agent_failover => true,
     allow_automatic_dhcp_failover    => true,
+    api_service_name                 => 'httpd'
     enable_proxy_headers_parsing     => $register_loadbalancer,
+    service_name                     => false,
     service_providers                => $service_providers,
     sync_db                          => $sync_db,
+  }
+
+  if($register_loadbalancer) {
+    $logformat = 'forwarded'
+  } else {
+    $logformat = false
+  }
+
+  class { '::neutron::wsgi::apache':
+    access_log_format => $logformat,
+    ssl               => false,
   }
 }
