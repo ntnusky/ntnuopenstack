7ce90a52c5f88bfb38bd955a257e44f8779000ba
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-24 13:25:32 +0100

    Start logging glance-api from apache

--- manifests/glance/logging.pp
+++ manifests/glance/logging.pp
@@ -1,5 +1,8 @@
 # Configures logging for glance 
 class ntnuopenstack::glance::logging {
+  # The API-service is logged through apache 
+  include ::profile::services::apache::logging
+
   ntnuopenstack::common::logging { 'glance-api':
     project => 'glance',
   }
