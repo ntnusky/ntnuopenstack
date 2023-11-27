df76c37cdb53288e2dbb9684882bc56912d549b4
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-27 15:35:52 +0100

    The count_usage_from_placement parameter is now set by ::nova

--- manifests/nova/services/base.pp
+++ manifests/nova/services/base.pp
@@ -1,17 +1,7 @@
 # Performs basic nova configuration for various services (api, schedulers etc).
 class ntnuopenstack::nova::services::base {
-  # Determine if quotas should be counted through placement
-  $placement_quota = lookup('ntnuopenstack::nova::quota::placement', {
-    'value_type'    => Boolean,
-    'default_value' => true,
-  })
-
   include ::ntnuopenstack::nova::common::base
   include ::ntnuopenstack::nova::common::cache
   require ::ntnuopenstack::nova::dbconnection
   require ::ntnuopenstack::repo
-
-  nova_config {
-    'quota/count_usage_from_placement': value => $placement_quota;
-  }
 }
