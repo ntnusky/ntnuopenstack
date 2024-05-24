f950d061d1928b2dd7f2e41180cbb304265eb437
Eigil Obrestad <eigil-git@obrestad.org>
2024-05-24 09:04:45 +0200

    Let puppet configure the nova hypervisor ID

---
+++ manifests/nova/compute/id.pp
@@ -0,0 +1,19 @@
+# Configures the nova hypervisor ID
+class ntnuopenstack::nova::compute::id {
+  $ids = lookup('ntnuopenstack::nova::compute::ids', {
+    'value_type' => Hash[String, String],
+  })
+
+  if(! $::fqdn in $ids) {
+    fail("The hostname ${::fqdn} is missing in the " +
+      "'ntnuopenstack::nova::compute::ids' key in hiera")
+  }
+
+  file { '/var/lib/nova/compute_id':
+    ensure  => 'file',
+    content => $ids[$::fqdn],
+    group   => 'nova',
+    owner   => 'nova',
+    mode    => '0640',
+  }
+}
