603c940ed8530b04693f6ad1c3c139568e684b7a
Eigil Obrestad <eigil-git@obrestad.org>
2024-05-24 14:37:54 +0200

    Make the disting of sudo-config optional

--- manifests/glance/sudo.pp
+++ manifests/glance/sudo.pp
@@ -1,8 +1,20 @@
 # This class configures sudo for glance
 class ntnuopenstack::glance::sudo {
-  sudo::conf { 'glance_sudoers':
-    ensure         => 'present',
-    source         => 'puppet:///modules/ntnuopenstack/sudo/glance_sudoers',
-    sudo_file_name => 'glance_sudoers',
+  # if purge::unmanaged is true we purge all files in sudoers.d that is not 
+  # managed by puppet. In this case we need to distribute «a» version of the
+  # sudo-config for openstack-services to work. It is though better to use the
+  # packaged version, so if the purge::unmanaged flag is set to false we will
+  # not delete the packed version.
+  $purge = lookup('profile::baseconfig::sudo::purge::unmanaged', {
+    'default_value' => true,
+    'value_type'    => Boolean,
+  })
+
+  if($purge) {
+    sudo::conf { 'glance_sudoers':
+      ensure         => 'present',
+      source         => 'puppet:///modules/ntnuopenstack/sudo/glance_sudoers',
+      sudo_file_name => 'glance_sudoers',
+    }
   }
 }
--- manifests/nova/api/sudo.pp
+++ manifests/nova/api/sudo.pp
@@ -1,8 +1,20 @@
 # Configures sudo to allow administration of the novaapi.
 class ntnuopenstack::nova::api::sudo {
-  sudo::conf { 'novaapi_sudoers':
-    ensure         => 'present',
-    source         => 'puppet:///modules/ntnuopenstack/sudo/novaapi_sudoers',
-    sudo_file_name => 'novaapi_sudoers',
+  # if purge::unmanaged is true we purge all files in sudoers.d that is not 
+  # managed by puppet. In this case we need to distribute «a» version of the
+  # sudo-config for openstack-services to work. It is though better to use the
+  # packaged version, so if the purge::unmanaged flag is set to false we will
+  # not delete the packed version.
+  $purge = lookup('profile::baseconfig::sudo::purge::unmanaged', {
+    'default_value' => true,
+    'value_type'    => Boolean,
+  })
+
+  if($purge) {
+    sudo::conf { 'novaapi_sudoers':
+      ensure         => 'present',
+      source         => 'puppet:///modules/ntnuopenstack/sudo/novaapi_sudoers',
+      sudo_file_name => 'novaapi_sudoers',
+    }
   }
 }
