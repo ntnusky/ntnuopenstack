c57fbf729dc8ff72033c1200960bfdf9e3c24160
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-28 12:59:10 +0200

    Create database-user for zabbix

---
+++ manifests/zabbix/database.pp
@@ -0,0 +1,35 @@
+# Configures a database-user for zabbix monitoring of openstack
+class ntnuopenstack::zabbix::database {
+  $username = lookup('ntnuopenstack::zabbix::database::username', {
+    'default_value' => 'zabbix_ro',
+    'value_type'    => String,
+  })
+  $password = lookup('ntnuopenstack::zabbix::database::password', {
+    'value_type'    => String,
+  })
+
+  $databases = [
+    'glance',
+    'keystone',
+    'magnum', 
+    'neutron',
+    'nova', 
+    'octavia',
+  ]
+
+  mysql_user { "${username}@%":
+    ensure        => 'present',
+    password_hash => mysql::password($password),
+  }
+
+  $databases.each | $database | {
+    mysql_grant { "${username}@%/${database}.*":
+      ensure     => 'present',
+      options    => ['GRANT'],
+      privileges => ['SELECT'],
+      table      => "${database}.*",
+      user       => "${username}@%",
+      require    => Mysql_user["${username}@%"],
+    }
+  }
+}
