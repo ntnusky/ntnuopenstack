bca7d30eee7a7e06e665ba7eb00b0cfda01075f7
Felix Albrigtsen <felix.albrigtsen@ntnu.no>
2024-09-30 13:13:33 +0200

    Designate: Format config, fix various module imports

--- manifests/designate.pp
+++ manifests/designate.pp
@@ -2,6 +2,5 @@
 class ntnuopenstack::designate {
   include ::ntnuopenstack::designate::api
   include ::ntnuopenstack::designate::backend
-  include ::ntnuopenstack::designate::base
   require ::ntnuopenstack::repo
 }
