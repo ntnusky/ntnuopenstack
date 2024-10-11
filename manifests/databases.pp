30a0b9902adc7b9ba5e52befa77baa4b6d6b4ce2
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-11 10:32:37 +0200

    Only create those databases that is needed

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -1,45 +1,26 @@
 # Defines the databases used by openstack
 class ntnuopenstack::databases {
-  include ::ntnuopenstack::cinder::database
-  include ::ntnuopenstack::glance::database
-  include ::ntnuopenstack::heat::database
-  include ::ntnuopenstack::keystone::database
-  include ::ntnuopenstack::neutron::database
-  include ::ntnuopenstack::nova::database
-  include ::ntnuopenstack::placement::database
-
-  $barbican = lookup('ntnuopenstack::barbican::mysql::password', {
-    'value_type'    => Variant[Boolean, String],
-    'default_value' => false,
-  })
-
-  $magnum = lookup('ntnuopenstack::magnum::mysql::password', {
-    'value_type'    => Variant[Boolean, String],
-    'default_value' => false,
-  })
-
-  $octavia = lookup('ntnuopenstack::octavia::mysql::password', {
-    'value_type'    => Variant[Boolean, String],
-    'default_value' => false,
-  })
-
   $zabbixservers = lookup('profile::zabbix::agent::servers', {
     'default_value' => [],
     'value_type'    => Array[Stdlib::IP::Address::Nosubnet],
   })
 
-  if($barbican) {
-    include ::ntnuopenstack::barbican::database
+  # Create databases for the openstack-services needed in the current region.
+  # What services that are needed is determined by which services have gotten a
+  # database password in hiera.
+  $services = ['barbican', 'cinder', 'glance', 'heat', 'keystone', 'magnum',
+    'neutron', 'nova', 'octavia', 'placement', ]
+  $services.each | $service | {
+    $password = lookup("ntnuopenstack::${service}::mysql::password", {
+      'default_value' => undef,
+      'value_type'    => Optional[String],
+    })
+    if($password) {
+      include "::ntnuopenstack::${service}::database"
+    }
   }
 
-  if($magnum) {
-    include ::ntnuopenstack::magnum::database
-  }
-
-  if($octavia) {
-    include ::ntnuopenstack::octavia::database
-  }
-
+  # If we are monitoring this platform with zabbix, ensure 
   if($zabbixservers =~ Array[Stdlib::IP::Address::Nosubnet, 1]) {
     include ::ntnuopenstack::zabbix::database
   }
