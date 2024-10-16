9223168d3915fac518e883f94686b54e92b30cf2
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-16 08:25:11 +0200

    Use the password

--- manifests/magnum/endpoint.pp
+++ manifests/magnum/endpoint.pp
@@ -13,7 +13,7 @@ define ntnuopenstack::magnum::endpoint (
     admin_url    => "${adminurl}:9511/v1",
     auth_name    => $username,
     internal_url => "${internalurl}:9511/v1",
-    password     => $keystone_password,
+    password     => $password,
     public_url   => "${publicurl}:9511/v1",
     region       => $region,
   }
