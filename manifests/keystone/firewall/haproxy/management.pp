df5d762a0f91b4e0745697f5df983132c2071ec1
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-16 08:59:41 +0200

    All regions need to reach the internal keystone endpoints

--- manifests/keystone/firewall/haproxy/management.pp
+++ manifests/keystone/firewall/haproxy/management.pp
@@ -1,6 +1,6 @@
 # Configures the firewall to allow regular keystone from the infra-net
 class ntnuopenstack::keystone::firewall::haproxy::management {
-  ::profile::firewall::infra::region { 'Keystone-Internal':
+  ::profile::firewall::infra::all { 'Keystone-Internal':
     port => 5000,
   }
 }
