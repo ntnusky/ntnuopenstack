f811fbb0ca36f9e6823b767b249d0f281cb23791
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-16 15:18:41 +0200

    Get placement-password from the right place

--- manifests/nova/common/placement.pp
+++ manifests/nova/common/placement.pp
@@ -1,12 +1,15 @@
 # Configures nova to use the placement service 
 class ntnuopenstack::nova::common::placement {
-  $keystone = lookup('ntnuopenstack::endpoint::admin')
-  $password = lookup('ntnuopenstack::placement::keystone::password')
+  $services = lookup('ntnuopenstack::services', {
+    'value_type' => Hash[String, Hash[String, Variant[Hash, String]]],
+  })
+  $auth_url = lookup('ntnuopenstack::keystone::auth::url')
   $region = lookup('ntnuopenstack::region')
 
   class { '::nova::placement':
-    password    => $password,
-    auth_url    => "${keystone}:5000/v3",
+    password    =>
+      $services[$region]['services']['placement']['keystone']['password'],
+    auth_url    => $auth_url, 
     region_name => $region,
   }
 }
