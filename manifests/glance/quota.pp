1cbd463161e38c3178ad270b32a7bad41a9d4f02
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-16 14:49:41 +0200

    Syntax-errors

--- manifests/glance/quota.pp
+++ manifests/glance/quota.pp
@@ -3,7 +3,7 @@ class ntnuopenstack::glance::quota {
   $services = lookup('ntnuopenstack::services', {
     'value_type' => Hash[String, Hash[String, Variant[Hash, String]]],
   })
-  $keystone_internal = lookup('ntnuopenstack::keystone::endpoint::internal', 
+  $keystone_internal = lookup('ntnuopenstack::keystone::endpoint::internal',
       Stdlib::Httpurl)
   $region = lookup('ntnuopenstack::region', String)
   $use_keystone_limits = lookup('ntnuopenstack::glance::keystone::limits', {
@@ -18,11 +18,11 @@ class ntnuopenstack::glance::quota {
     })
 
     class { '::glance::limit':
-      auth_url     => "${keystone_internal}:5000",
-      endpoint_id  => $endpoint_id,
-      password     => 
-        $services[$region]['services']['glance']['keystone']['password'] 
-      region_name  => $region,
+      auth_url    => "${keystone_internal}:5000",
+      endpoint_id => $endpoint_id,
+      password    =>
+        $services[$region]['services']['glance']['keystone']['password'],
+      region_name => $region,
     }
   }
 }
