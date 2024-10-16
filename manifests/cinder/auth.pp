ac66a894d14487b959054456c2f6a848ac3490da
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-16 09:43:56 +0200

    Use the correct auth details for Cinder

--- manifests/cinder/auth.pp
+++ manifests/cinder/auth.pp
@@ -1,10 +1,12 @@
 # Configures the base cinder config
 class ntnuopenstack::cinder::auth {
-  $auth_url = lookup('ntnuopenstack::keystone::auth::url')
-  $password = lookup('ntnuopenstack::cinder::keystone::password')
-  $region_name = lookup('ntnuopenstack::region')
-  $www_authenticate_uri = lookup('ntnuopenstack::keystone::auth::uri')
+  $services = lookup('ntnuopenstack::services', {
+    'value_type' => Hash[String, Hash[String, Variant[Hash, String]]],
+  })
 
+  $auth_url = lookup('ntnuopenstack::keystone::auth::url')
+  $www_authenticate_uri = lookup('ntnuopenstack::keystone::auth::uri')
+  $region_name = lookup('ntnuopenstack::region')
   $memcache_servers = lookup('profile::memcache::servers', {
     'value_type'    => Array[Stdlib::IP::Address],
     'default_value' => [],
@@ -16,16 +18,22 @@ class ntnuopenstack::cinder::auth {
   class { '::cinder::keystone::authtoken':
     auth_url                     => $auth_url,
     memcached_servers            => $memcache,
-    password                     => $password,
+    password                     =>
+      $services[$region_name]['services']['cinder']['keystone']['password'],
     region_name                  => $region_name,
     service_token_roles_required => true,
+    username                     =>
+      $services[$region_name]['services']['cinder']['keystone']['username'],
     www_authenticate_uri         => $www_authenticate_uri,
   }
 
   class { '::cinder::keystone::service_user':
     auth_url                => $auth_url,
-    password                => $password,
+    password                =>
+      $services[$region_name]['services']['cinder']['keystone']['password'],
     region_name             => $region_name,
     send_service_user_token => true,
+    username                =>
+      $services[$region_name]['services']['cinder']['keystone']['username'],
   }
 }
