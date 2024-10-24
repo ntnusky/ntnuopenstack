a72d05910157d93498f183d6fb5b84d9b418da94
Felix Albrigtsen <felix.albrigtsen@ntnu.no>
2024-10-24 10:45:43 +0200

    Designate: Extend dns firewall rules

--- manifests/designate/firewall/dns.pp
+++ manifests/designate/firewall/dns.pp
@@ -1,7 +1,12 @@
 # Configures firewall rules for the designate workers outward facing DNS
 class ntnuopenstack::designate::firewall::dns {
-  ::profile::baseconfig::firewall::service::global { 'designate worker outward facing DNS':
+  ::profile::baseconfig::firewall::service::global { 'designate worker outward facing DNS(TCP)':
     protocol => 'tcp',
+    port     => 53,
+  }
+
+  ::profile::baseconfig::firewall::service::global { 'designate worker outward facing DNS(UDP)':
+    protocol => 'udp',
     port     => 53,
   }
 }
