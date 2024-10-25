e7a5cc1dfd94597557a174a08d9dc7b4d0a61779
Felix Albrigtsen <felix.albrigtsen@ntnu.no>
2024-10-25 10:37:26 +0200

    Designate: Configure backend to use the publishing NS and allow rndc

--- manifests/designate/backend.pp
+++ manifests/designate/backend.pp
@@ -1,6 +1,21 @@
 # Designate DNS Backend (bind9)
 class ntnuopenstack::designate::backend {
   class {'::designate::backend::bind9':
+    ns_records       => {
+      1 => 'ns1.ntnu.no.',
+      2 => 'ns2.ntnu.no.',
+    },
+    rndc_config_file => '/etc/rndc.conf',
+    rndc_key_file    => '/etc/bind/rndc.key'
+  }
 
+  file { '/etc/rndc.conf':
+    ensure => present,
+    owner  => 'root',
+    group  => 'bind',
+    mode   => '0644',
+    source =>
+      'puppet:///modules/ntnuopenstack/designate/rndc.conf',
+    before => Anchor['designate::service::begin'],
   }
 }
