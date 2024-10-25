501f770b4bd84bdf072faf40f09ea6e33abc0393
Felix Albrigtsen <felix.albrigtsen@ntnu.no>
2024-10-25 11:10:33 +0200

    Designate: Allow sudo designate-rootwrap

--- manifests/designate/api.pp
+++ manifests/designate/api.pp
@@ -5,6 +5,7 @@ class ntnuopenstack::designate::api {
   require ::ntnuopenstack::designate::base
   require ::ntnuopenstack::designate::auth
   contain ::ntnuopenstack::designate::haproxy::backend
+  include ::ntnuopenstack::designate::sudo
 
   class { '::designate::api':
     auth_strategy    => 'keystone',
