14d8be5d87e3f4006094060d0b96a02375892ab3
Felix Albrigtsen <felix.albrigtsen@ntnu.no>
2024-11-15 14:37:18 +0100

    Designate: Make rndc.conf template

--- manifests/designate/backend.pp
+++ manifests/designate/backend.pp
@@ -1,29 +1,29 @@
 # Designate interop with DNS Backend
 class ntnuopenstack::designate::backend {
+  $ns_servers = lookup('ntnuopenstack::designate::ns_servers', Array[String]);
+
   class {'::designate::backend::bind9':
     ns_records       => lookup('ntnuopenstack::designate::ns_records', Hash[Integer, String]),
 
     rndc_config_file => '/etc/rndc.conf',
     rndc_key_file    => '/etc/rndc.key',
 
-    bind9_hosts      => lookup('ntnuopenstack::designate::ns_servers', Array[String]),
-    nameservers      => lookup('ntnuopenstack::designate::ns_servers', Array[String]),
+    bind9_hosts      => $ns_servers,
+    nameservers      => $ns_servers,
     configure_bind   => false,
-  }
-
-  file { '/etc/rndc.conf':
-    ensure => present,
-    owner  => 'root',
-    group  => 'designate',
-    mode   => '0644',
-    source =>
-      'puppet:///modules/ntnuopenstack/designate/rndc.conf',
-    before => Anchor['designate::service::begin'],
   }
 
   $rndc_algorithm = 'hmac-md5';
   $rndc_name = 'rndc-key';
   $rndc_secret = lookup('ntnuopenstack::designate::rndc_key', String);
+
+  file { '/etc/rndc.conf':
+    ensure  => file,
+    owner   => 'designate',
+    group   => 'designate',
+    mode    => '0644',
+    content => template('ntnuopenstack/designate/rndc-conf.erb'),
+  }
 
   file { '/etc/rndc.key':
     ensure  => file,
