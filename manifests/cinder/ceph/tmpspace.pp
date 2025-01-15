607225747ab32739bdc5fce24abe1f7f1675ec25
Eigil Obrestad <eigil-git@obrestad.org>
2025-01-15 13:57:12 +0100

    Create a rbd-volume for temporary volume-storage for cinder

---
+++ manifests/cinder/ceph/tmpspace.pp
@@ -0,0 +1,44 @@
+# Creates a ceph image and mounts it so that cinder can store images there
+# during conversions.
+class ntnuopenstack::cinder::ceph::tmpspace {
+  $tmpsize = lookup('ntnuopenstack::cinder::tmpspace::gigabytes', {
+    'default_value' => 1000,
+    'value_type'    => Integer,
+  })
+
+  include ::cinder::deps
+  require ::profile::ceph::client
+
+  $megabytes = 1024 * $tmpsize
+  $imagename = "rbd/cinder-tmp-${::hostname}"
+  exec { 'Create RBD tmpspace':
+    command => "/usr/bin/rbd create --size ${megabytes} ${imagename}",
+    unless  => "/usr/bin/rbd info ${imagename}",
+  }
+
+  ::profile::ceph::rbdmap { 'Cinder-tmpspace':
+    image   => $imagename,
+    keyring => '/etc/ceph/ceph.client.admin.keyring',
+    user    => 'admin',
+    require => Exec['Create RBD tmpspace'],
+  }
+
+  exec { 'Format the tmpspace':
+    command => "/sbin/mkfs.ext4 /dev/rbd/${imagename}",
+    unless  => "/sbin/blkid -t TYPE=ext4 /dev/rbd/${imagename}",
+    require => Profile::Ceph::Rbdmap['Cinder-tmpspace'],
+  }
+
+  mount { '/var/lib/cinder/conversion':
+    ensure  => 'mounted',
+    device  => "/dev/rbd/${imagename}",
+    require => Exec['Format the tmpspace'],
+  }
+
+  file { '/var/lib/cinder/conversion':
+    ensure => directory,
+    group  => 'cinder',
+    owner  => 'cinder',
+    mode   => '0750',
+  }
+}
