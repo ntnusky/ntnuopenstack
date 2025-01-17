91921fc94a61a55a6c16ba3657d6f34674c662b7
Eigil Obrestad <eigil-git@obrestad.org>
2025-01-17 08:49:36 +0100

    Dont waith on rbd during boot. The volume gets available later

--- manifests/cinder/ceph/tmpspace.pp
+++ manifests/cinder/ceph/tmpspace.pp
@@ -35,6 +35,7 @@ class ntnuopenstack::cinder::ceph::tmpspace {
   mount { '/var/lib/cinder/conversion':
     ensure  => 'mounted',
     fstype  => 'ext4',
+    options => 'nobootwait',
     device  => "/dev/rbd/${imagename}",
     require => Exec['Format the tmpspace'],
   }
