317e3f769d3c2c79e7ccb7a9065dd45fca7279f2
Eigil Obrestad <eigil-git@obrestad.org>
2025-01-24 14:10:07 +0100

    Use the services-data instead of the deprecated password-keys to determine which services are present.

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -1,26 +1,27 @@
 # Defines the databases used by openstack
 class ntnuopenstack::databases {
+  $region = lookup('ntnuopenstack::region')
+  $services = lookup('ntnuopenstack::services', {
+    'value_type' => Hash[String, Hash],
+  })
   $zabbixpw = lookup('ntnuopenstack::zabbix::database::password', {
     'default_value' => undef,
     'value_type'    => Optional[String],
   })
 
   # Create databases for the openstack-services needed in the current region.
-  # What services that are needed is determined by which services have gotten a
-  # database password in hiera.
+  # What services that are needed is determined by which services have gotten
+  # data in hiera.
   $services = ['barbican', 'cinder', 'glance', 'heat', 'keystone', 'magnum',
     'neutron', 'nova', 'octavia', 'placement', ]
   $services.each | $service | {
-    $password = lookup("ntnuopenstack::${service}::mysql::password", {
-      'default_value' => undef,
-      'value_type'    => Optional[String],
-    })
-    if($password) {
+    if($region in $services and $service in $services[$region]['services']) {
       include "::ntnuopenstack::${service}::database"
     }
   }
 
-  # If we are monitoring this platform with zabbix, ensure 
+  # If we are monitoring this platform with zabbix, create a user with
+  # read-access to the databases for zabbix. 
   if($zabbixpw) {
     include ::ntnuopenstack::zabbix::database
   }
