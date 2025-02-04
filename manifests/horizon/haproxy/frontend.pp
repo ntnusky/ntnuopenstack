f1de78093fe0127e2627404283d2692614d4b974
Eigil Obrestad <eigil-git@obrestad.org>
2025-02-04 15:48:43 +0100

    Let haproxy-servers override which regions to collect servers from per backend.

--- manifests/horizon/haproxy/frontend.pp
+++ manifests/horizon/haproxy/frontend.pp
@@ -30,13 +30,25 @@ class ntnuopenstack::horizon::haproxy::frontend {
       'default_value' => undef,
       'value_type'    => Optional[String],
     })
-    $region = lookup('profile::haproxy::region', {
-      'default_value' => $region_fallback,
-      'value_type'    => String,
+    $overrides = lookup('profile::haproxy::region::override', {
+      'default_value' => {},
+      'value_type'    => Hash[String, Array[String]],
     })
 
-    Haproxy::Balancermember <<| listening_service == 'bk_horizon' and
-        tag == "region-${region}" |>>
+    # If there is defined an override-list for a certain haproxy-backend, use
+    # that list as the list of regions to collect servers from.
+    if("bk_${name}" in $overrides) {
+      $regions = [] + $overrides['bk_horizon']
+
+    # Otherwise use the haproxy-servers region
+    } else {
+      $regions = [ $region_fallback ]
+    }
+
+    $regions.each | $region | {
+      Haproxy::Balancermember <<| listening_service == 'bk_horizon' and
+          tag == "region-${region}" |>>
+    }
   }
 
 }
