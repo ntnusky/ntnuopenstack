9c857d4d09b0f1c28007b14afa8cc02d13490ced
Eigil Obrestad <eigil-git@obrestad.org>
2025-02-19 09:35:48 +0100

    Dont sync DB by default; we want to control this manually.

--- manifests/designate/dbconnection.pp
+++ manifests/designate/dbconnection.pp
@@ -1,11 +1,16 @@
 # Configures designate to use its database
 class ntnuopenstack::designate::dbconnection {
   # Determine database-settings
+  $dbsync = lookup('ntnuopenstack::designate::mysql::sync', {
+    'default_value' => false,
+    'value_type'    => Boolean,
+  })
   $mysql_pass = lookup('ntnuopenstack::designate::mysql::password', String)
   $mysql_ip = lookup('ntnuopenstack::designate::mysql::ip', Stdlib::IP::Address)
   $database_connection = "mysql+pymysql://designate:${mysql_pass}@${mysql_ip}/designate"
 
   class { '::designate::db':
     database_connection => $database_connection,
+    sync_db             => $dbsync,
   }
 }
