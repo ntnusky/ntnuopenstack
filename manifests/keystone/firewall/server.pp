5ec77e75ebf58993d6a493b1f15b308724451fa9
Eigil Obrestad <eigil-git@obrestad.org>
2025-02-28 16:02:13 +0100

    Allow keystone to be interregional

--- manifests/keystone/firewall/server.pp
+++ manifests/keystone/firewall/server.pp
@@ -2,7 +2,19 @@
 #  - We basicly only allow our management-network to use keystone, as the rest
 #    of the traffic will enter trough the loadbalancers.
 class ntnuopenstack::keystone::firewall::server {
-  ::profile::firewall::infra::region { 'Keystone-API':
-    port => 5000,
+  $common = lookup('ntnuopenstack::keystone::interregional', {
+    'default_value' => false,
+    'value_type'    => Boolean,
+  })
+
+  if($common) {
+    ::profile::firewall::infra::all { 'Keystone-API':
+      port => 5000,
+    }
+  } else {
+    ::profile::firewall::infra::region { 'Keystone-API':
+      port => 5000,
+    }
   }
+
 }
