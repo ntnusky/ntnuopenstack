4abb402132771ccba69f355bc28956a71bfe84ca
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-07-04 14:07:27 +0200

    mDNS can be both tcp and udp

--- manifests/designate/firewall/mdns.pp
+++ manifests/designate/firewall/mdns.pp
@@ -1,7 +1,13 @@
 # Configures firewall rules for mdns for designate::api
 class ntnuopenstack::designate::firewall::mdns {
   ::profile::firewall::custom { 'designate mdns':
-    hiera_key => 'ntnuopenstack::designate::ns_servers',
-    port      => 5354,
+    hiera_key          => 'ntnuopenstack::designate::ns_servers',
+    port               => 5354,
+    transport_protocol => 'tcp',
+  }
+  ::profile::firewall::custom { 'designate mdns':
+    hiera_key          => 'ntnuopenstack::designate::ns_servers',
+    port               => 5354,
+    transport_protocol => 'udp',
   }
 }
