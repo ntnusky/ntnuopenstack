11f32b9d55e6473185b28f7a843e9072b7f3f779
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-07-04 14:10:11 +0200

    Unique names is nice

--- manifests/designate/firewall/mdns.pp
+++ manifests/designate/firewall/mdns.pp
@@ -1,11 +1,11 @@
 # Configures firewall rules for mdns for designate::api
 class ntnuopenstack::designate::firewall::mdns {
-  ::profile::firewall::custom { 'designate mdns':
+  ::profile::firewall::custom { 'designate mdns TCP':
     hiera_key          => 'ntnuopenstack::designate::ns_servers',
     port               => 5354,
     transport_protocol => 'tcp',
   }
-  ::profile::firewall::custom { 'designate mdns':
+  ::profile::firewall::custom { 'designate mdns UDP':
     hiera_key          => 'ntnuopenstack::designate::ns_servers',
     port               => 5354,
     transport_protocol => 'udp',
