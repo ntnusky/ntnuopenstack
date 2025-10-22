d57f2ed6ef5b5d76da6b0d8f1ada77a33a6fc6a1
Eigil Obrestad <eigil-git@obrestad.org>
2025-10-22 10:52:30 +0200

    First bits of multiple BGP speakers on the same host

---
+++ manifests/neutron/bgp/agent.pp
@@ -0,0 +1,38 @@
+# Configures a neutron BGP agent
+class ntnuopenstack::neutron::bgp::agent (
+  $interface,
+) {
+  $bgpconf = "/etc/neutron/bgp_dragent_${name}.ini"
+  $bgplog = "/var/log/neutron/neutron-bgp-dragent-${name}.log"
+
+  $router_id = $::sl2['server']['interfaces'][$interface]['ipv4']
+
+  lookup('ntnuopenstack::neutron::bgp::speaker::driver', {
+    'default_value' => 'neutron_dynamic_routing.services.bgp.agent.driver.os_ken.driver.OsKenBgpDriver',
+    'value_type'    => String,
+  })
+
+  ini_setting { "BGP-Agent ${name} router-id":
+    ensure  => present,
+    path    => $bgpconf,
+    section => 'bgp',
+    setting => 'bgp_router_id',
+    value   => $router_id,
+    tag     => [
+      'neutron-dragent-bgp-config',
+      "neutron-dragent-bgp-config-${name}",
+    ],
+  }
+
+  ini_setting { "BGP-Agent ${name} bgp_speaker_driver":
+    ensure  => present,
+    path    => $bgpconf,
+    section => 'bgp',
+    setting => 'bgp_speaker_driver',
+    value   => $speaker_driver,
+    tag     => [
+      'neutron-dragent-bgp-config',
+      "neutron-dragent-bgp-config-${name}",
+    ],
+  }
+}
---
+++ manifests/neutron/bgp/multi.pp
@@ -0,0 +1,28 @@
+# Installs and configures multiple bgp routing agents 
+class ntnuopenstack::neutron::bgp::multi {
+  require ::ntnuopenstack::neutron::base
+  include ::ntnuopenstack::neutron::logging::dragent
+  require ::ntnuopenstack::repo
+  include ::ntnuopenstack::zabbix::neutron::bgp
+
+  include ::neutron::deps
+  include ::neutron::params
+
+  $agents = lookup('ntnuopenstack::neutron::bgp::agents', {
+    'default_value' => {},
+    'value_type'    => Hash[String, String],
+  })
+
+  $agents.each | $agentname, $data | {
+    ::ntnuopenstack::neutron::bgp::agent { $agentname:
+      interface => $data['interface'],
+      require   => Package['neutron-bgp-dragent'],
+    }
+  }
+
+  package { 'neutron-bgp-dragent':
+    ensure => present,
+    name   => $::neutron::params::bgp_dragent_package,
+    tag    => ['openstack', 'neutron-package'],
+  }
+}
