edce22ef8ae6fcf2c4c6f0b3b39b4e84bd4b3717
Eigil Obrestad <eigil-git@obrestad.org>
2026-04-17 10:32:34 +0200

    Replace legacy fact

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,11 +3,11 @@
 # when we run the default OpenStack version for the current Ubuntu Release
 # set 'openstack_extras::repo::debian::ubuntu::manage_uca: false' in hiera if needed
 class ntnuopenstack::repo {
-  if ($::osfamily == 'Debian' and $::operatingsystem == 'Ubuntu') {
+  if ($::facts['os']['name'] == 'Ubuntu') {
     class { '::openstack_extras::repo::debian::ubuntu':
       package_require => true,
     }
   } else {
-    fail("Operating system family ${::osfamily} is not supported.")
+    fail("Operating system family ${::facts['os']['name']} is not supported.")
   }
 }
